e31f302a280bb271e94292f51d974d7317d404ba
RicRibeiroTri <ricardoribeirotrindade273@gmail.com>
2025-04-17 22:49:16 -0300

    Update and rename Exercício11.java to Exercicio11.java

--- Exercicio11.java
+++ Exercicio11.java
@@ -1,9 +1,9 @@
-package Exercícios;
+package exercicios;
 
 import java.util.Arrays;
 import java.util.Random;
 
-public class Exercício11 {
+public class Exercicio11 {
 
 	public static void main(String[] args) {
 		
